9ccd9af1d622eaec8727965b1b7edbcbab9e7cd1
Karl Kwon <kkwon@in4ins.com>
2018-03-22 01:16:36 -0400

    Bar Charts Completed

--- bar/app.js
+++ bar/app.js
@@ -5,24 +5,57 @@
 // });
 
 var data = [];
-for (var i = 0; i < 20; i++) {
+for (var i = 0; i < 30; i++) {
   // var num = Math.floor(Math.random() * 50);
-  num = Math.floor(d3.randomUniform(1,50)());
+  num = Math.floor(d3.randomUniform(1, 50)());
   data.push(num);
 }
 console.log(data);
-generate(data);
 
+// Create SVG Element
+var chart_width = 800;
+var chart_height = 400;
+var bar_padding = 5;
+var svg = d3.select('#chart')
+  .append('svg')
+  .attr('width', chart_width)
+  .attr('height', chart_height);
 
-function generate(data){
-  d3.select('#chart')
-  .selectAll('div')
+// Bind Data and Create Bars
+svg.selectAll('rect')
   .data(data)
   .enter()
-  .append('div')
-  .attr('class','bar')
-  .style('height', function(d) {
-    var height = d * 3;
-    return height + 'px'
+  .append('rect')
+  .attr('x', function(d, i) {
+    return i * (chart_width / data.length);
   })
-}
+  .attr('y', function(d) {
+    return chart_height - d * 5;
+  })
+  .attr('width', function() {
+    return chart_width / data.length - bar_padding;
+  })
+  .attr('height', function(d) {
+    return d * 5;
+  })
+  .attr('fill', '#71adde');
+
+
+// Create Labels
+svg.selectAll('text')
+  .data(data)
+  .enter()
+  .append('text')
+  .text(function(d) {
+    return d
+  })
+  .attr('x', function(d, i) {
+    return i * (chart_width / data.length) +
+      (chart_width / data.length - bar_padding) / 2;
+  })
+  .attr('y', function(d) {
+    return chart_height - d * 5 + 15;
+  })
+  .attr('font-size', 12)
+  .attr('fill', '#ffffff')
+  .attr('text-anchor', 'middle')
