fceab40da296a3e2b8acbd8ed57a7a10c06cc061
Karl Kwon <kkwon@in4ins.com>
2018-03-22 00:18:03 -0400

    Add heigh style with funnction (d)

--- bar/app.js
+++ bar/app.js
@@ -1,9 +1,18 @@
 // var data = [10, 20, 30, 40, 50];
 
-d3.json('data.json').then(function(data) {
-  console.log(data);
-  generate(data);
-});
+// d3.json('data.json').then(function(data) {
+//   console.log(data);
+// });
+
+var data = [];
+for (var i = 0; i < 20; i++) {
+  // var num = Math.floor(Math.random() * 50);
+  num = Math.floor(d3.randomUniform(1,50)());
+  data.push(num);
+}
+console.log(data);
+generate(data);
+
 
 function generate(data){
   d3.select('#chart')
@@ -11,8 +20,7 @@ function generate(data){
   .data(data)
   .enter()
   .append('div')
-  .classed('bar', true)
-  // .attr('class','bar')
+  .attr('class','bar')
   .style('height', function(d) {
     var height = d * 3;
     return height + 'px'
